ee4239f7df01e353bc93a4be1d5e8e449d81ee22
Donny Ramadhan <45101977+donnyramadhan-dr@users.noreply.github.com>
2020-04-08 21:34:03 +0700

    how to make data yourself by echo in php

---
+++ aboutme.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Nama    : Donny Ramadhan<br>";
+echo "Kelas   : XI RPL 1 <br>";
+echo "Sekolah : SMK Taruna Bhakti <br>";
+echo "Alamat  : Jl.Kp.Tipar Tengah ,Rt.04/Rw.10 No.90";
+
+?>
