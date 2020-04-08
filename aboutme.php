4dcb351688d0d699b2ec7373ec916616d3a953df
Donny Ramadhan <45101977+donnyramadhan-dr@users.noreply.github.com>
2020-04-08 21:39:44 +0700

    delete alamat, add no hp and email

--- aboutme.php
+++ aboutme.php
@@ -3,6 +3,7 @@
 echo "Nama    : Donny Ramadhan<br>";
 echo "Kelas   : XI RPL 1 <br>";
 echo "Sekolah : SMK Taruna Bhakti <br>";
-echo "Alamat  : Jl.Kp.Tipar Tengah ,Rt.04/Rw.10 No.90";
+echo "No HP   : 0895411983870<br>";
+echo "Email   : ramadhandonny60@gmail.com";
 
 ?>
